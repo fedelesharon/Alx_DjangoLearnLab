466c3aba37946948d7d520299ec02f927b0730e7
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-11-15 08:30:14 +0300

    django files

--- django-models/django-models/urls.py
+++ django-models/django-models/urls.py
@@ -20,3 +20,19 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# relationship_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
+# django_models/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),
+]
