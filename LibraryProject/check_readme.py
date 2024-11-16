eb35277605d833aee656fd8f141b5bc85300626a
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-11-16 21:40:41 +0300

    files

--- LibraryProject/check_readme.py
+++
@@ -1,8 +0,0 @@
-import os
-
-file_path = "LibraryProject\README.md"
-
-if os.path.isfile(file_path) and os.path.getsize(file_path) > 0:
-    print("README.md file is non-empty.")
-else:
-    print("README.md file is empty or does not exist.")
