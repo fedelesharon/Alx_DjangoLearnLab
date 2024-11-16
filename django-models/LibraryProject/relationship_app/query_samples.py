63ef804455740c720b1687ca8d5ca3f23e2bc536
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-11-16 22:06:12 +0300

    my django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,11 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+author_name = Author.objects.get(name='Mike')
+Author.objects.get(name=author_name)
+
+books = Book.objects.all()
+books.all()
+
+library_name = 'Mike  Absai Library'
+library = Library.objects.get(name=library_name)
+Librarian.objects.get(library=library)
