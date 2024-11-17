c638acab9e746d8eb551845216fccd95bdff3009
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-11-17 15:37:16 +0300

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+
 
 # Create your views here.
 def list_books(request):
@@ -27,3 +29,26 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# Helper functions to check roles
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html', {'role': 'Admin'})
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html', {'role': 'Librarian'})
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html', {'role': 'Member'})
