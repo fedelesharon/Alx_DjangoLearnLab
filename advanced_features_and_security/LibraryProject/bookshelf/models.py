cb57f43674c397536bc785122184f618e6cb2ede
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-11-21 10:14:26 +0300

    files

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,3 +5,21 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn_number = models.CharField(max_length=13)
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField()
+
+    def __str__(self):
+        return self.name
