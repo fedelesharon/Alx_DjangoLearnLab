35e43a01ef0879854f0fadcb64f3e2c680b741ce
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-12-03 18:45:15 +0300

    django

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email', request.user.email)
+        request.user.save()
+    return render(request, 'blog/profile.html', {'user': request.user})
