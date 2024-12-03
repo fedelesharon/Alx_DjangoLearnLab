42ca4e714da55dfcf1dec95a47845e05c52cc70f
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-12-03 18:57:50 +0300

    django

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path("post/<int:pk>/update/")
 
 ]
